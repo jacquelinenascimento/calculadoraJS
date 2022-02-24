142c08f98f208bf290d7719fcc6904d5823dc303
Jacque <jacque.tdsn@gmail.com>
2022-02-24 20:37:31 -0300

    calculadora em JS

---
+++ tiposdefuncao.js
@@ -0,0 +1,15 @@
+/*function funcao() {
+    console.log("Olá, jovem!")
+}
+
+funcao();*/
+
+/*var funcao = function() {
+    console.log("Teste de funcoes")
+} 
+funcao();*/
+
+var funcao = () => {
+    console.log("Arrow => function")
+}
+funcao();
